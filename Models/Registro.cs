cddeff524bf8e248294d9f93e8edab824d107789
AValentinoGPereda <angel_garcia3@usmp.pe>
2023-04-21 09:52:41 -0500

    Dbeaver data

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace myapp.Models
 {
-    [Table("registro")]
+    [Table("usuario")]
     public class Registro
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
