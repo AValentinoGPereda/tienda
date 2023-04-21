4398f7f7f0195b87e514ad51029c4efe2924a8d1
Gemies11 <gerardo_escarate@usmp.pe>
2023-04-21 01:05:16 -0500

    Avance de render gerardo

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -1,20 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using myapp.Models;
+using myapp.Models;
+using myapp.Data;
 
 namespace myapp.Controllers;
 
 public class RegistroController : Controller {
      private readonly ILogger<RegistroController> _logger;
+      private readonly ApplicationDbContext _context;
 
-    public RegistroController(ILogger<RegistroController> logger)
+    
+     public RegistroController(ILogger<RegistroController> logger,
+         ApplicationDbContext context)
     {
         _logger = logger;
+         _context = context;
     }
 
-    public IActionResult Registrar()
+    public IActionResult Registro()
     {
         return View("Registro");
+        
     }
 
     public IActionResult InicioSesion()
@@ -27,4 +34,12 @@ public class RegistroController : Controller {
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+      [HttpPost]
+        public IActionResult Registro(Registro objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "El contacto ya esta registrado";
+            return View();
+        }
 }
