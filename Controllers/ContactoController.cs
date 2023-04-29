3faba8cfe0d39c72461f9582e4931198cb3e49bc
AValentinoGPereda <angel_garcia3@usmp.pe>
2023-04-28 20:56:04 -0500

    catalogo v2

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using myapp.Models;
+using myapp.Data;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+
+namespace myapp.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ILogger<ContactoController> logger,
+         ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "El contacto ya esta registrado";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
